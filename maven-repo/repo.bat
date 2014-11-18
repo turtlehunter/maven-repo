17da9b64cc56464400b86aede7d7fac343fd72cc
turtlehunter <urielsalisgaming@gmail.com>
2014-11-18 19:26:18 -0300

    Changed repo.bat and added checksums

--- maven-repo/repo.bat
+++ maven-repo/repo.bat
@@ -1,3 +1,2 @@
-rem usage repo groupid artifactid version jarfile jar true
 
-mvn install:install-file -DgroupId=%1 -DartifactId=%2 -Dversion=%3 -Dfile=%4 -Dpackaging=%5 -DgeneratePom=%6 -DlocalRepositoryPath=./repository
+mvn install:install-file -DcreateChecksum=true -DgroupId=%1 -DartifactId=%2 -Dversion=%3 -Dfile=%4 -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=./repository
